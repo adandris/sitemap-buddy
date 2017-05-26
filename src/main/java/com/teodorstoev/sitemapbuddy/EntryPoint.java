7acf1f4d3adc5fe1ddad1527c3b70e058e832a8e
adandris <teodor.stoev@gmail.com>
2017-05-26 22:20:01 +0200

    PageCrawler adjustments: HTTP request timeout, limited concurrent connections.

--- src/main/java/com/teodorstoev/sitemapbuddy/EntryPoint.java
+++ src/main/java/com/teodorstoev/sitemapbuddy/EntryPoint.java
@@ -41,8 +41,7 @@ public class EntryPoint {
         String siteUrl = commandLine.getOptionValue(OPTION_URL);
         String path = commandLine.getOptionValue(OPTION_OUTPUT);
 
-        Vertx vertx = Vertx.vertx(
-                new VertxOptions().setWorkerPoolSize(2).setMaxWorkerExecuteTime(TimeUnit.SECONDS.toNanos(15)));
+        Vertx vertx = Vertx.vertx(new VertxOptions().setWorkerPoolSize(2));
 
         deployVerticles(vertx, onDeploy -> createSitemap(vertx, siteUrl, sitemap -> {
             List<PageInfo> urlSet = new ArrayList<>(sitemap.size());
@@ -86,7 +85,8 @@ public class EntryPoint {
     }
 
     private static void createSitemap(Vertx vertx, String siteUrl, Consumer<JsonArray> consumer) {
-        vertx.eventBus().send(Events.MAP_SITE, siteUrl, new DeliveryOptions().setSendTimeout(120000), event -> {
+        vertx.eventBus().send(Events.MAP_SITE, siteUrl,
+                new DeliveryOptions().setSendTimeout(TimeUnit.MINUTES.toMillis(5)), event -> {
             if (event.succeeded()) {
                 JsonArray result = (JsonArray) event.result().body();
                 consumer.accept(result);
--- src/main/java/com/teodorstoev/sitemapbuddy/components/PageCrawler.java
+++ src/main/java/com/teodorstoev/sitemapbuddy/components/PageCrawler.java
@@ -3,8 +3,6 @@ package com.teodorstoev.sitemapbuddy.components;
 import com.teodorstoev.sitemapbuddy.domain.Events;
 import com.teodorstoev.sitemapbuddy.domain.PageInfo;
 import io.vertx.core.AbstractVerticle;
-import io.vertx.core.AsyncResult;
-import io.vertx.core.Handler;
 import io.vertx.core.eventbus.Message;
 import io.vertx.core.json.JsonArray;
 import org.jsoup.Connection;
@@ -24,6 +22,8 @@ import java.time.format.DateTimeParseException;
  * Created by adandris on 10.05.17.
  */
 public class PageCrawler extends AbstractVerticle {
+    private int concurrentRequestCount;
+
     @Override
     public void start() {
         vertx.eventBus().consumer(Events.CRAWL_PAGE, message -> {
@@ -31,34 +31,45 @@ public class PageCrawler extends AbstractVerticle {
             System.out.println("Crawling " + urlToCrawl + " ...");
             try {
                 URI uri = new URI(urlToCrawl);
-                String baseUrl = uri.getHost();
 
-                executeRequest(uri, event -> {
-                    if (event.succeeded()) {
-                        Connection.Response response = event.result();
-
-                        parseResponseAndReply(message, baseUrl, response);
-                    } else {
-                        event.cause().printStackTrace();
-                        message.fail(1, event.cause().getMessage());
-                    }
-                });
+                executeNowOrLater(message, uri);
             } catch (URISyntaxException e) {
                 e.printStackTrace();
             }
         });
     }
 
-    private void executeRequest(URI uri, Handler<AsyncResult<Connection.Response>> resultHandler) {
-        vertx.executeBlocking(event -> {
+    private void executeNowOrLater(Message<Object> message, URI uri) {
+        if(concurrentRequestCount < 10) {
+            concurrentRequestCount++;
+            requestAndParse(uri, message);
+        } else {
+            vertx.setTimer(1000, handler -> executeNowOrLater(message, uri));
+        }
+    }
+
+    private void requestAndParse(URI uri, Message<Object> message) {
+        vertx.<Connection.Response>executeBlocking(event -> {
             try {
                 String decodedUrl = URLDecoder.decode(uri.toString(), "UTF-8");
-                Connection.Response response = Jsoup.connect(decodedUrl).execute();
+                Connection.Response response = Jsoup.connect(decodedUrl).timeout(15000).execute();
                 event.complete(response);
             } catch (Exception e) {
                 event.fail(e);
             }
-        }, false, resultHandler);
+        }, false, event -> {
+            concurrentRequestCount--;
+
+            if (event.succeeded()) {
+                Connection.Response response = event.result();
+
+                String baseUrl = uri.getHost();
+                parseResponseAndReply(message, baseUrl, response);
+            } else {
+                event.cause().printStackTrace();
+                message.fail(1, event.cause().getMessage());
+            }
+        });
     }
 
     private void parseResponseAndReply(Message<Object> message, String baseUrl, Connection.Response response) {
